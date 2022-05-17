239fa9f208508329681f68a01614583188549720
Souvik Saha <sosah@microsoft.com>
2022-05-17 22:59:00 +0530

    adding tsconfig to automate js file creation

--- javascript/Queue.js
+++ javascript/Queue.js
@@ -1,29 +1,29 @@
 export class Queue {
-	constructor(cap) {
-		this.q = new Array(cap);
-		this.front = 0;
-		this.rear = cap - 1;
-		this.cap = cap;
-		this.size = 0;
-	}
-	push(val) {
-		if (this.size === this.cap) {
-			throw new Error('Queue is full!');
-		}
-		this.rear = (this.rear + 1) % this.cap;
-		this.q[this.rear] = val;
-		this.size++;
-	}
-	isEmpty() {
-		return this.size === 0;
-	}
-	pop() {
-		if (this.isEmpty()) {
-			throw new Error('Queue is empty!');
-		}
-		const val = this.q[this.front];
-		this.front = (this.front + 1) % this.cap;
-		this.size--;
-		return val;
-	}
+    constructor(cap) {
+        this.q = new Array(cap);
+        this.front = 0;
+        this.rear = cap - 1;
+        this.cap = cap;
+        this.size = 0;
+    }
+    push(val) {
+        if (this.size === this.cap) {
+            throw new Error('Queue is full!');
+        }
+        this.rear = (this.rear + 1) % this.cap;
+        this.q[this.rear] = val;
+        this.size++;
+    }
+    isEmpty() {
+        return this.size === 0;
+    }
+    pop() {
+        if (this.isEmpty()) {
+            throw new Error('Queue is empty!');
+        }
+        const val = this.q[this.front];
+        this.front = (this.front + 1) % this.cap;
+        this.size--;
+        return val;
+    }
 }
